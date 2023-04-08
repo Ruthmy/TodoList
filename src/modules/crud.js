00bbb2c8213e23cf20e04d15c8a056682341b7c4
Ruthmy <Ruryemo.exe@gmail.com>
2023-04-08 17:23:25 -0600

    Finish details with the interface

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -1,23 +1,18 @@
-//Get HTML elements
-const tasksList = document.getElementById('list');
+// Get HTML elements
 const addButton = document.getElementById('add-button');
 const input = document.getElementById('add-item');
-const deleteButton = document.getElementById('clear-button');
-const listItem = document.querySelectorAll('.item');
 
 // Tasks on the local storage
 let ItemsArray = JSON.parse(localStorage.getItem('tasks')) || [];
 
 function displayTasks() {
-  // Get the <ul> element
+  // Get the ul element
   const list = document.querySelector('#list');
-
-  // Remove any existing <li> elements
   list.innerHTML = '';
 
-  // Loop through the items and add them to the <ul>
+  // Loop through the items and add them to the ul
   ItemsArray.forEach((task) => {
-    // Create the <li> element
+    // Create the li element
     const li = document.createElement('li');
     li.classList.add('item');
 
@@ -39,18 +34,14 @@ function displayTasks() {
     // Create the edit button
     const editButton = document.createElement('button');
     editButton.classList.add('btn');
-    editButton.setAttribute('aria-label', 'Edit');
-    editButton.innerHTML = '&#9998;';
     li.appendChild(editButton);
 
     // Create the delete button
     const deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn');
-    deleteButton.setAttribute('aria-label', 'Delete');
-    deleteButton.innerHTML = '&#128465;';
+    deleteButton.classList.add('btn-delete');
     li.appendChild(deleteButton);
 
-    // Add the <li> to the <ul>
+    // Add the li to the ul
     list.appendChild(li);
 
     // Add event listeners for editing and deleting
@@ -58,11 +49,12 @@ function displayTasks() {
       // Replace the label with an input field
       const input = document.createElement('input');
       input.setAttribute('type', 'text');
+      input.setAttribute('class', 'edit');
       input.value = task.description;
       li.replaceChild(input, label);
 
       // Change the edit button to a save button
-      editButton.innerHTML = '&#128190;';
+      editButton.classList.replace('btn', 'delete');
 
       // Set focus on the input field
       input.focus();
@@ -74,37 +66,27 @@ function displayTasks() {
 
           // Update the task description in the array
           task.description = input.value;
-
-          // Save the updated array to localStorage
           localStorage.setItem('tasks', JSON.stringify(ItemsArray));
 
           // Replace the input field with the label
           li.replaceChild(label, input);
 
           // Change the save button back to an edit button
-          editButton.innerHTML = '&#9998;';
-
-          // Redisplay the list of tasks
+          editButton.classList.replace('btn', 'delete');
           displayTasks();
         }
       });
-      
+
       deleteButton.addEventListener('click', () => {
-        // Remove the task from the array
         ItemsArray.splice(task.index - 1, 1);
-  
+
         // Update the index property of the remaining tasks
         ItemsArray.forEach((task, index) => {
           task.index = index + 1;
         });
-        
-        // Save the updated ItemsArray to the localStorage
-      localStorage.setItem('tasks', JSON.stringify(ItemsArray));
-
-      // Reload the page to reflect the changes
-      window.location.reload();
+        localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+        window.location.reload();
       });
-
     });
 
     checkbox.addEventListener('change', (event) => {
@@ -112,14 +94,11 @@ function displayTasks() {
       ItemsArray[index].completed = event.target.checked;
       localStorage.setItem('tasks', JSON.stringify(ItemsArray));
     });
-    
-
   });
 }
 
 // Add functionality
 function addTask() {
-  // Get the task from the input element
   const input = document.getElementById('add-item');
   const description = input.value;
   const index = ItemsArray.length + 1;
@@ -127,14 +106,12 @@ function addTask() {
   const newItem = { index, description, completed };
   ItemsArray.push(newItem);
   localStorage.setItem('tasks', JSON.stringify(ItemsArray));
-  // Reset form values
   document.getElementById('add-form').reset();
   window.location.reload();
 }
 
 // Add a new task when user hit the add button
 addButton.addEventListener('click', (event) => {
-  // Prevents the form from being sent
   event.preventDefault();
   addTask();
 });
@@ -146,24 +123,20 @@ input.addEventListener('keydown', (event) => {
   }
 });
 
+// Delete the completed tasks
 function deleteCompletedTasks() {
-  // Remove completed tasks from array
-  ItemsArray = ItemsArray.filter(task => !task.completed);
+  ItemsArray = ItemsArray.filter((task) => !task.completed);
 
   // Update the index property of remaining tasks
   ItemsArray.forEach((task, index) => {
     task.index = index + 1;
   });
-
-  // Save updated array to localStorage
   localStorage.setItem('tasks', JSON.stringify(ItemsArray));
-
-  // Redisplay the list of tasks
   displayTasks();
 }
 
 const clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', function() {
+clearButton.addEventListener('click', () => {
   deleteCompletedTasks();
 });
 
