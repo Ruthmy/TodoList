363206c4f0758db1fbfc020beaea79477aa8dcf8
Ruthmy <Ruryemo.exe@gmail.com>
2023-04-08 14:33:37 -0600

    Refactored the code, just need the change when a checkbox is clicked

---
+++ src/modules/crud.js
@@ -0,0 +1,169 @@
+//Get HTML elements
+const tasksList = document.getElementById('list');
+const addButton = document.getElementById('add-button');
+const input = document.getElementById('add-item');
+const deleteButton = document.getElementById('clear-button');
+const listItem = document.querySelectorAll('.item');
+
+// Tasks on the local storage
+let ItemsArray = JSON.parse(localStorage.getItem('tasks')) || [];
+
+function displayTasks() {
+  // Get the <ul> element
+  const list = document.querySelector('#list');
+
+  // Remove any existing <li> elements
+  list.innerHTML = '';
+
+  // Loop through the items and add them to the <ul>
+  ItemsArray.forEach((task) => {
+    // Create the <li> element
+    const li = document.createElement('li');
+    li.classList.add('item');
+
+    // Create the <input> element
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('name', 'item');
+    checkbox.setAttribute('id', `item-${task.index}`);
+    checkbox.setAttribute('data-index', task.index);
+    checkbox.checked = task.completed;
+    li.appendChild(checkbox);
+
+    // Create the <label> element
+    const label = document.createElement('label');
+    label.setAttribute('for', `item-${task.index}`);
+    label.textContent = task.description;
+    li.appendChild(label);
+
+    // Create the edit button
+    const editButton = document.createElement('button');
+    editButton.classList.add('btn');
+    editButton.setAttribute('aria-label', 'Edit');
+    editButton.innerHTML = '&#9998;';
+    li.appendChild(editButton);
+
+    // Create the delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn');
+    deleteButton.setAttribute('aria-label', 'Delete');
+    deleteButton.innerHTML = '&#128465;';
+    li.appendChild(deleteButton);
+
+    // Add the <li> to the <ul>
+    list.appendChild(li);
+
+    // Add event listeners for editing and deleting
+    label.addEventListener('dblclick', () => {
+      // Replace the label with an input field
+      const input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      input.value = task.description;
+      li.replaceChild(input, label);
+
+      // Change the edit button to a save button
+      editButton.innerHTML = '&#128190;';
+
+      // Set focus on the input field
+      input.focus();
+
+      // Add event listener to save changes on enter or blur
+      input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.keyCode === 13 || event.type === 'blur') {
+          event.preventDefault();
+
+          // Update the task description in the array
+          task.description = input.value;
+
+          // Save the updated array to localStorage
+          localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+
+          // Replace the input field with the label
+          li.replaceChild(label, input);
+
+          // Change the save button back to an edit button
+          editButton.innerHTML = '&#9998;';
+
+          // Redisplay the list of tasks
+          displayTasks();
+        }
+      });
+      
+      deleteButton.addEventListener('click', () => {
+        // Remove the task from the array
+        ItemsArray.splice(task.index - 1, 1);
+  
+        // Update the index property of the remaining tasks
+        ItemsArray.forEach((task, index) => {
+          task.index = index + 1;
+        });
+        
+        // Save the updated ItemsArray to the localStorage
+      localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+
+      // Reload the page to reflect the changes
+      window.location.reload();
+      });
+
+      const checkbox = document.querySelector('input[name="item"]');
+      checkbox.addEventListener('change', (event) => {
+        const index = event.target.dataset.index - 1;
+        toggleCompleted(index);
+      });
+      
+    });
+
+  });
+}
+
+// Add functionality
+function addTask() {
+  // Get the task from the input element
+  const input = document.getElementById('add-item');
+  const description = input.value;
+  const index = ItemsArray.length + 1;
+  const completed = false;
+  const newItem = { index, description, completed };
+  ItemsArray.push(newItem);
+  localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+  // Reset form values
+  document.getElementById('add-form').reset();
+  window.location.reload();
+}
+
+// Add a new task when user hit the add button
+addButton.addEventListener('click', (event) => {
+  // Prevents the form from being sent
+  event.preventDefault();
+  addTask();
+});
+
+// Add a new task when user hit enter key
+input.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
+
+function deleteCompletedTasks() {
+  // Remove completed tasks from array
+  ItemsArray = ItemsArray.filter(task => !task.completed);
+
+  // Update the index property of remaining tasks
+  ItemsArray.forEach((task, index) => {
+    task.index = index + 1;
+  });
+
+  // Save updated array to localStorage
+  localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+
+  // Redisplay the list of tasks
+  displayTasks();
+}
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', function() {
+  deleteCompletedTasks();
+});
+
+displayTasks();
--- src/modules/items.js
+++ src/modules/items.js
@@ -8,7 +8,6 @@ const listItem = document.querySelectorAll('.item');
 let ItemsArray = JSON.parse(localStorage.getItem('tasks')) || [];
 
 // Render the items
-
 let insideItem = '';
 ItemsArray.forEach((singleItemContents, i) => {
   // Update the index of the item in the array
@@ -18,17 +17,15 @@ ItemsArray.forEach((singleItemContents, i) => {
     <li class="item">
         <input value="${singleItemContents.index}" name="item" type="checkbox" data-index="${singleItemContents.index}" id="${singleItemContents.index}">
         <label for="0${singleItemContents.index}">${singleItemContents.description}</label>
-        <button type="button" class="btn delete" aria-label="Delete" id="${singleItemContents.index}" data-index="${singleItemContents.index}"></button>
+        <button type="button" class="btn" aria-label="Delete" id="${singleItemContents.index}" data-index="${singleItemContents.index}"></button>
     </li>
     `;
 });
-
 const section = document.querySelector('.list');
 section.innerHTML = insideItem;
 
-// This function is to change the completed status on the array items
+// Change the completed status on the array items
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
-
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('change', (event) => {
     const index = parseInt(event.target.value, 10);
@@ -47,21 +44,14 @@ function addTask() {
   // Get the task from the input element
   const input = document.getElementById('add-item');
   const description = input.value;
-  // Set and index base on the actual array
   const index = ItemsArray.length + 1;
   const completed = false;
-  // Create teh object
   const newItem = { index, description, completed };
-  // Save the task
   ItemsArray.push(newItem);
-  // Save on local storage
   localStorage.setItem('tasks', JSON.stringify(ItemsArray));
   // Reset form values
   document.getElementById('add-form').reset();
   window.location.reload();
-
-  // To update the listed tasks
-  const taskList = document.getElementById('library');
 }
 
 // Add a new task when user hit the add button
@@ -92,7 +82,6 @@ function clearCompleted() {
 const clearButton = document.querySelector('#clear-button');
 clearButton.addEventListener('click', clearCompleted);
 
-
 //To edit the task
 const labels = document.querySelectorAll('label');
 labels.forEach((label) => {
@@ -108,7 +97,7 @@ labels.forEach((label) => {
     const input = event.target.querySelector('input');
     input.focus();
 
-    // Update the task's description on blur (when the user clicks outside the input element)
+    // Update the task's description when the user clicks outside the input element
     input.addEventListener('blur', () => {
       const newDescription = input.value;
 
@@ -118,8 +107,6 @@ labels.forEach((label) => {
       // Update the task's description on the ItemsArray
       const task = ItemsArray.find((task) => task.index === index);
       task.description = newDescription;
-
-      // Save the updated ItemsArray to the localStorage
       localStorage.setItem('tasks', JSON.stringify(ItemsArray));
 
       // Replace the input element with the updated label
@@ -130,8 +117,69 @@ labels.forEach((label) => {
 
 // Change button class to a trash can icon when the user hovers over a task
 const taskItems = document.querySelectorAll('.item');
+taskItems.forEach((item, index) => {
+  const button = item.querySelector('.btn');
+  item.addEventListener('mouseover', () => {
+    button.classList.remove('btn');
+    button.classList.add('delete');
+    button.classList.add('red');
+  });
+  item.addEventListener('mouseout', () => {
+    button.classList.remove('delete');
+    button.classList.remove('red');
+    button.classList.add('btn');
+  });
+  
+});
+// /////////////////////////////////////////////////////
+
+/*
+item.addEventListener('click', () => {
+  //const realIndex = index + 1;
+  removeTask(index);
+}); 
+*/
+
+// Remove a book from the collection
+  const removeTask = (index) => {
+    let itemToRemove = ItemsArray[index];
+    console.log(itemToRemove);
+    const newItemsArray = ItemsArray.filter(item => item !== itemToRemove);
+    console.log(newItemsArray);
+    ItemsArray = newItemsArray;
+    // Guardar el array actualizado en el localStorage
+    localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+    window.location.reload();
+};
+
+
+/*
+// Filter out completed items
+const newItemsArray = ItemsArray.filter((item) => !item.completed);
+
+// Update local storage
+ItemsArray = newItemsArray;
+localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+window.location.reload();
+*/
+
+
+/*
+window.addEventListener('load', () => {
+  if (localStorage.getItem('tasks')) {
+    ItemsArray = JSON.parse(localStorage.getItem('tasks'));
+  } else {
+    ItemsArray = [];
+  }
+});
+*/
+
+/*
+// Change button class to a trash can icon when the user hovers over a task
+const taskItems = document.querySelectorAll('.item');
 taskItems.forEach((item) => {
   const button = item.querySelector('.btn');
+  console.log(button);
   item.addEventListener('mouseover', () => {
     button.classList.remove('btn');
     button.classList.add('delete');
@@ -142,44 +190,33 @@ taskItems.forEach((item) => {
   });
 });
 
-
-/*
-//To edit the task
-const labels = document.querySelectorAll('label');
-labels.forEach((label) => {
-  label.addEventListener('dblclick', (event) => {
-    // Get the current label's text content and store it in a variable
-    const currentIndex = event.target.previousElementSibling.value;
-    const currentDescription = event.target.textContent;
-
-    // Replace the label's HTML with an input element
-    event.target.innerHTML = `<input type="text" value="${currentDescription}" />`;
-
-    // Get the input element inside the label and focus on it
-    const input = event.target.querySelector('input');
-    input.focus();
-
-    // Update the task's description on blur (when the user clicks outside the input element)
-    input.addEventListener('blur', () => {
-      const newDescription = input.value;
-
-      // Get the task's index
-      const index = parseInt(input.parentElement.previousElementSibling.value, 10);
-
-      // Update the task's description on the ItemsArray
-      const task = ItemsArray.find((task) => task.index === index);
-      task.description = newDescription;
-
-      // Save the updated ItemsArray to the localStorage
-      localStorage.setItem('tasks', JSON.stringify(ItemsArray));
-
-      // Replace the input element with the updated label
-      event.target.innerHTML = newDescription;
-    });
-  });
-});
 */
 
+
+/*
+// Delete the task
+function removeTask(event) {
+  // Get the index of the task
+  const index = parseInt(event.target.id, 10);
+
+  // Find the task in the array
+  const taskIndex = ItemsArray.findIndex((task) => task.index === index);
+
+  // Remove the task from the array
+  ItemsArray.splice(taskIndex, 1);
+
+  // Update the index of the remaining tasks
+  ItemsArray.forEach((task, i) => {
+    task.index = i + 1;
+  });
+
+  // Save the updated ItemsArray to the localStorage
+  localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+
+  // Reload the page to reflect the changes
+  window.location.reload();
+}
+*/
 
 /*
 // Remove a task from the array
