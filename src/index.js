54d6a2068ef5817a0bcdedac773661bc3e2d8213
Ruthmy <Ruryemo.exe@gmail.com>
2023-04-08 19:22:27 -0600

    Splited the code into another module for the deleteCompletedTasks function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './styles.css';
 import './modules/crud.js';
+import { deleteCompletedTasks } from './modules/deleteCompletedTasks';
+
---
+++ src/modules/deleteCompletedTasks.js
@@ -0,0 +1,15 @@
+// deleteTasks.js
+function deleteCompletedTasks(ItemsArray) {
+    ItemsArray = ItemsArray.filter((task) => !task.completed);
+  
+    // Update the index property of remaining tasks
+    ItemsArray.forEach((task, index) => {
+      task.index = index + 1;
+    });
+  
+    localStorage.setItem('tasks', JSON.stringify(ItemsArray));
+    return ItemsArray;
+  }
+  
+  export { deleteCompletedTasks };
+  
